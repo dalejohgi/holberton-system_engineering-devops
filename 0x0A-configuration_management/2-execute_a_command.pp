edd287a8ce9909fb7d721a11a4a80926d89c80e6
David <1508@holbertonschool.com>
2020-08-03 22:37:15 +0000

    Fix lint requirements

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Killing a process using puppet
 
 exec { 'killmenow':
-    command  => 'pkill -f killmenow',
-    path     => ['/usr/bin'],
-    returns  => [0],
+    command => 'pkill -f killmenow',
+    path    => ['/usr/bin'],
+    returns => [0],
 }
