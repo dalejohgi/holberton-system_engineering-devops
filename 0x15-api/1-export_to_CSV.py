e977c9cf4bbdbe8cda02fd6a87949f1751b59e9a
David <1508@holbertonschool.com>
2020-09-14 21:06:07 -0500

    Add task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,16 +6,18 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
+    file_path = argv[1] + ".csv"
     api_url = "https://jsonplaceholder.typicode.com"
     user = requests.get('{}/users/{}'.format(api_url, argv[1])).json()
     tds = requests.get('{}/todos/?userId={}'.format(api_url, argv[1])).json()
     tds_done = requests.get('{}/todos/?userId={}&completed=true'
                             .format(api_url, argv[1])).json()
 
-    user_name = user.get('name')
-
-    print('Employee {} is done with tasks({}/{}):'
-          .format(user_name, len(tds_done), len(tds)))
-
-    for tasks in tds_done:
-        print('\t {}'.format(tasks.get('title')))
+    user_name = user.get('username')
+    with open(file_path, 'a') as f:
+        for tasks in tds:
+            f.write("\"{}\",\"{}\",\"{}\",\"{}\"\n"
+                    .format(argv[1],
+                            user_name,
+                            tasks.get('completed'),
+                            tasks.get('title')))
