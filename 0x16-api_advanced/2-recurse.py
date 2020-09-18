a47c49b353930ab490a470014cb056f896107ea3
David <1508@holbertonschool.com>
2020-09-17 21:20:40 -0500

    Add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,22 +4,22 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """Return a list of the hot subreddits or 0 for invalid subreddit"""
     if not subreddit:
         return None
     user_agent = {'User-Agent': 'dalejohgi'}
-    parameters = {'after': 'after', 'limit': 100}
+    parameters = {'after': after, 'limit': 100}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=user_agent, params=parameters)
     if response.status_code not in range(200, 300):
         return None
     response_json = response.json()
-    elements_dict = response_json['data']['children']
-    if len(response_json) == 0:
-        return (hot_list)
-    else:
-        for element in elements_dict:
+    after = response_json['data']['after']
+    elements = response_json['data']['children']
+    for element in elements:
             hot_list.append(element['data']['title'])
-        print(len(hot_list))
-        recurse(subreddit, hot_list)
+    if after is None:
+        return hot_list
+    else:
+        return (recurse(subreddit, hot_list, after))
