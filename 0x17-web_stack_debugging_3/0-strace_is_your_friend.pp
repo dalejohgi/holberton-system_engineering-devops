3859c75f7bc8690030877b2aff14ebd9f042fddf
David <1508@holbertonschool.com>
2020-09-29 16:06:12 -0500

    Add web debbuging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# find out why Apache is returning a 500 error
+# Apache is returning a 500 error SOLUTION
 exec {'fix php ext.':
   command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
