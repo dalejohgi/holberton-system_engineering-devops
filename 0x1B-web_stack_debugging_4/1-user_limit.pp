e18f4631c27a43ae94f6e5a7389746f42c35a114
David <1508@holbertonschool.com>
2020-10-08 19:19:54 -0500

    Add missing key

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
 #Configure broader limits
 exec { 'change-os-configuration-for-holberton-user':
   command => "/bin/sed -i -e 's/nofile 5/nofile 1000/g;s/nofile 4/nofile 1000/g' /etc/security/limits.conf"
+}
